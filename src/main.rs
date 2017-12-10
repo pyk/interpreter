bd49753997da67d1d2457934fcdc39eace509dc7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2017-12-10 17:53:22 +0700

    interpreter: Add stdin and stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        let stdin = io::stdin();
+        let mut stdout = io::stdout();
+
+        let interpreter_name = "ro> ".as_bytes();
+        stdout.write(interpreter_name).unwrap();
+        stdout.flush().unwrap();
+
+        stdin.read_line(&mut input).unwrap();
+        println!("{:?}", input);
+    }
 }
